2efe3fe7e5e35ea498c3fa228cacfb275e174fae
Eli Citron <ebcitron@gmail.com>
2019-08-20 13:41:32 -0500

    Re-doing things

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,22 @@
-// Test away!
+import React from 'react';
+import Display from './Display';
+import {render} from '@testing-library/react';
+
+
+
+describe('<Display />', () => {
+    it('renders without crashing', () => {
+        render(<Display />);
+    });
+    it('open and unlocked', () => {
+        const {getByText, queryByText } = render(<Display closed={false} locked={false} />);
+        const unlock = getByText(/unlocked/i);
+        const open = getByText(/closed/i);
+        
+        expect(unlock.className).toMatch(/\red-led\b/i);
+        
+        expect(queryByText(/closed/i)).toBe(null);
+
+
+    });
+});
