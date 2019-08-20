6f89ea711c98e6e1c6260fc2fccbf998acc0c94c
Eli Citron <ebcitron@gmail.com>
2019-08-20 13:54:25 -0500

    Updated

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import Display from './Display';
-import {render} from '@testing-library/react';
-
+import {render, cleanup} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
 
 describe('<Display />', () => {
     it('renders without crashing', () => {
         render(<Display />);
     });
-    it('open and unlocked', () => {
+    it('opened and unlocked', () => {
         const {getByText, queryByText } = render(<Display closed={false} locked={false} />);
         const unlock = getByText(/unlocked/i);
-        const open = getByText(/closed/i);
-        
-        expect(unlock.className).toMatch(/\red-led\b/i);
+        const open = getByText(/open/i);
+
+        expect(unlock).toHaveClass('green-led');
         
         expect(queryByText(/closed/i)).toBe(null);
+               
+        //expect(unlock.className).toMatch(/\red-led\b/i);
+        
+       // expect(queryByText(/closed/i)).toBe(null);
+    })
+        it('closed and unlocked', () => {
+            const {getByText} = render(<Display closed={true} locked={false} /> );
+            getByText(/unlocked/i);
+            getByText(/closed/i);
+        });
+        it('closed and locked', () => {
+            const {getByText} = render(<Display closed = {true} locked = {false} />);
+            getByText(/^locked$/i);
+            getByText(/closed/i);
 
 
+        })
     });
 });
